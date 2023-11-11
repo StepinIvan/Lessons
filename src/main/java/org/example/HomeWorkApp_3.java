3108f8067a6df5966c81e20388a960befcc173a2
StepinIvan <ivan.stepin.99@mail.ru>
2023-11-11 18:23:53 +0700

    Lesson - 3 homework (8)

--- src/main/java/org/example/HomeWorkApp_3.java
+++ src/main/java/org/example/HomeWorkApp_3.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class HomeWorkApp_3 {
@@ -83,10 +84,65 @@ public class HomeWorkApp_3 {
         System.out.println("tempMin " + tempMin);
         System.out.println("tempMax " + tempMax);
         System.out.println();
+
+        //7
+
+        int[] array_7 = {1, 1, 1, 2, 1};
+        System.out.println(checkBalance(array_7));
+        System.out.println();
+
+        //8
+
+        int[] array_8 = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(shaftArray(array_8, 1)));
     }
     public static int[] createArray (int length, int initialValue){
         int[] array_5 = new int[length];
         Arrays.fill(array_5, initialValue);
         return array_5;
+    }
+    public static boolean checkBalance(int[] array) {
+        boolean result = false;
+        int temp = array.length;
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < temp; j++) {
+                leftSum += array[j];
+            }
+            for (int k = temp; k < array.length; k++) {
+                rightSum += array[k];
+            }
+            temp--;
+            if (leftSum == rightSum) {
+                result = true;
+            }
+            leftSum = 0;
+            rightSum = 0;
+        }
+        return result;
+    }
+    public static int[] shaftArray (int[] array, int shaft) {
+        int temp;
+        if (shaft > 0) {
+            for (int i = 0; i < shaft; i++) {
+                temp = array[0];
+                array[0] = array[array.length - 1];
+                for (int j = array.length; j > 2; j--) {
+                    array[j - 1] = array[j - 2];
+                }
+                array[1] = temp;
+            }
+        } else {
+            for (int i = 0; i > shaft; i--) {
+                temp = array[array.length - 1];
+                array[array.length - 1] = array[0];
+                for (int j = 0; j < array.length - 2; j++) {
+                    array[j] = array[j + 1];
+                }
+                array[array.length - 2] = temp;
+            }
+        }
+        return array;
     }
 }
